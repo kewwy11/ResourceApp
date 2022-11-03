b35440e181e47577b06ee9c686fcf72beebc719e
kewwy11 <erhisohwodegratefulruki@gmail.com>
2022-11-03 23:40:16 +0100

    first commit

---
+++ src/components/learning-resource/StoredResources.vue
@@ -0,0 +1,33 @@
+<template>
+ <ul>
+        <list-resources v-for="res in resources" 
+        :key="res.id" 
+        :id="res.id"
+        :title="res.title" 
+        :description="res.description" 
+        :link="res.link">
+        </list-resources>
+    </ul>
+</template>
+
+<script>
+    import ListResources from './ListResources.vue'
+
+
+    export default{
+        components:{
+        ListResources
+    },
+        inject:['resources']
+    }
+</script>
+
+<style>
+    ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
